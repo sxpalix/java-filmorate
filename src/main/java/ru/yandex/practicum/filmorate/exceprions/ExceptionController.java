60db2f5827f2672d2b51decbce11bf6e0e5ed84d
sxpalix <vanessfrolov@gmail.com>
2023-07-24 11:26:27 +0300

    "Made changes in accordance with the terms of reference"

--- src/main/java/ru/yandex/practicum/filmorate/exceprions/ExceptionController.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceprions/ExceptionController.java
@@ -1,12 +1,9 @@
 package ru.yandex.practicum.filmorate.exceprions;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.Arrays;
 import java.util.Map;
 
 @Slf4j
