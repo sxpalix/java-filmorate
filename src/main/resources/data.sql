bce4bdca1507bf447a745f9afbdb60a02f141d6b
sxpalix <vanessfrolov@gmail.com>
2023-08-12 17:54:32 +0300

    "Checkstyle commit"

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,10 +3,22 @@ INSERT INTO GENRE(id, name) VALUES (default, 'Драма');
 INSERT INTO GENRE(id, name) VALUES (default, 'Мультфильм');
 INSERT INTO GENRE(id, name) VALUES (default, 'Триллер');
 INSERT INTO GENRE(id, name) VALUES (default, 'Документальный');
+INSERT INTO GENRE(id, name) VALUES (default, 'Комедия');
+INSERT INTO GENRE(id, name) VALUES (default, 'Драма');
+INSERT INTO GENRE(id, name) VALUES (default, 'Мультфильм');
+INSERT INTO GENRE(id, name) VALUES (default, 'Триллер');
+INSERT INTO GENRE(id, name) VALUES (default, 'Документальный');
 INSERT INTO GENRE(id, name) VALUES (default, 'Боевик');
+
 
 INSERT INTO MPA(id, name) VALUES (default, 'G');
 INSERT INTO MPA(id, name) VALUES (default, 'PG');
 INSERT INTO MPA(id, name) VALUES (default, 'PG-13');
 INSERT INTO MPA(id, name) VALUES (default, 'R');
 INSERT INTO MPA(id, name) VALUES (default, 'NC-17');
+
+
+[
+│ {"id":6,"name":"G"},{"id":7,"name":"PG"},{"id":8,"na
+  │ me":"PG-13"},{"id":9,"name":"R"},{"id":10,"name":"NC-1
+  │ 7"}]
