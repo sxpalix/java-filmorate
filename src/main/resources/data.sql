b1cbbb0ed4340ec8250482295830765d3b64b249
sxpalix <vanessfrolov@gmail.com>
2023-08-12 17:59:57 +0300

    "Checkstyle commit"

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,23 +3,8 @@ INSERT INTO GENRE(id, name) VALUES (default, 'Драма');
 INSERT INTO GENRE(id, name) VALUES (default, 'Мультфильм');
 INSERT INTO GENRE(id, name) VALUES (default, 'Триллер');
 INSERT INTO GENRE(id, name) VALUES (default, 'Документальный');
-INSERT INTO GENRE(id, name) VALUES (default, 'Комедия');
-INSERT INTO GENRE(id, name) VALUES (default, 'Драма');
-INSERT INTO GENRE(id, name) VALUES (default, 'Мультфильм');
-INSERT INTO GENRE(id, name) VALUES (default, 'Триллер');
-INSERT INTO GENRE(id, name) VALUES (default, 'Документальный');
-INSERT INTO GENRE(id, name) VALUES (default, 'Боевик');
-
-
 INSERT INTO MPA(id, name) VALUES (default, 'G');
 INSERT INTO MPA(id, name) VALUES (default, 'PG');
 INSERT INTO MPA(id, name) VALUES (default, 'PG-13');
 INSERT INTO MPA(id, name) VALUES (default, 'R');
 INSERT INTO MPA(id, name) VALUES (default, 'NC-17');
-INSERT INTO MPA(id, name) VALUES (default, 'G');
-INSERT INTO MPA(id, name) VALUES (default, 'PG');
-INSERT INTO MPA(id, name) VALUES (default, 'PG-13');
-INSERT INTO MPA(id, name) VALUES (default, 'R');
-INSERT INTO MPA(id, name) VALUES (default, 'NC-17');
-
-
