ac7b1f517d4bdaa00283f1bfa4b970453eb6b564
Artem Smirnov <kaban1106@yandex.ru>
2023-09-01 04:27:32 +0300

    Добавлен поиск фильмов по описанию и режиссёру

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmSearchController.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exceprions.IncorrectValuesException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.filmSearch.FilmSearchService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/films")
+@AllArgsConstructor
+public class FilmSearchController {
+	FilmSearchService service;
+
+	@GetMapping(path = "/search")
+	public List<Film> searchFilm(@RequestParam String query, @RequestParam List<String> by) throws IncorrectValuesException {
+		log.info("GET Request. Search films");
+		return service.searchFilm(query, by);
+	}
+
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/filmSearch/DbFilmSearchService.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.service.filmSearch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exceprions.IncorrectValuesException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.db.FilmSearchDbStorage;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class DbFilmSearchService implements FilmSearchService {
+
+	private final FilmSearchDbStorage storage;
+
+	@Autowired
+	public DbFilmSearchService(FilmSearchDbStorage storage) {
+		this.storage = storage;
+	}
+
+	@Override
+	public List<Film> searchFilm(String query, List<String> by) throws IncorrectValuesException {
+		log.info("Search films. query = {}; by = {}", query, by);
+		return storage.searchFilm(query, by);
+	}
+}
